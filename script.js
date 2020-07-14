3dbc877f027284c5b15cce7fef529560db13ad58
Frederick D <FrederickDelta@gmail.com>
2020-07-14 19:30:28 +0800

    📦 NEW: Set board hover

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const X_CLASS = "x";
 const CIRCLE_CLASS = "circle";
+const board = document.getElementById("board");
 const cellElements = document.querySelectorAll("[data-cell]");
 let circleTurn;
 
-cellElements.forEach((cell) => {
-  // Only fire click event once
-  cell.addEventListener("click", handleClick, { once: true });
-});
+startGame();
+function startGame() {
+  cellElements.forEach((cell) => {
+    // Only fire click event once
+    cell.addEventListener("click", handleClick, { once: true });
+  });
+
+  circleTurn = true;
+  setBoardHoverClass();
+}
 
 function handleClick(e) {
   // Place the mark
@@ -15,4 +22,16 @@ function handleClick(e) {
   cell.classList.add(currentClass);
   // Switch turns
   circleTurn = !circleTurn;
+
+  setBoardHoverClass();
+}
+
+function setBoardHoverClass() {
+  board.classList.remove(X_CLASS);
+  board.classList.remove(CIRCLE_CLASS);
+  if (circleTurn) {
+    board.classList.add(CIRCLE_CLASS);
+  } else {
+    board.classList.add(X_CLASS);
+  }
 }
